371e378c206c3ef7a627d46fc26fc9f12f271fd1
victor-kiptoo-001 <veelangat680@gmail.com>
2025-07-01 19:37:09 +0300

    loader isssue

---
+++ ecommerce_frontend/src/components/Loader.tsx
@@ -0,0 +1,25 @@
+const Loader = () => {Add commentMore actions
+  return (
+    <section className="loader">
+      <div></div>
+    </section>
+  );
+};
+
+export default Loader;
+
+interface SkeletonProps {
+  width?: string;
+  length?: number;
+}
+
+export const Skeleton = ({ width = "unset", length = 3 }: SkeletonProps) => {
+  const skeleton = Array.from({ length }, (_, idx) => (
+    <div key={idx} className="skeleton-shape"></div>
+  ));
+  return (
+    <div className="skeleton-loader" style={{ width }}>
+      {skeleton}
+    </div>
+  );
+};
